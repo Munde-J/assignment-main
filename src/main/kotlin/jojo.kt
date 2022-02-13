34bf12a4e17f250b6f0655197ef8f53683db2bab
Munde-J <mundeangelah@gmail.com>
2022-02-13 19:36:33 +0300

    first kotlin assignment

---
+++ src/main/kotlin/jojo.kt
@@ -0,0 +1,21 @@
+fun main() {
+    var name = ("angela Munde")
+    println (name)
+
+    var age = 25
+    println(age)
+
+    var weight = 66
+    println(weight)
+
+    var phonenumber = "0721829426"
+    println(phonenumber)
+
+    val kenyan = true
+    println(kenyan)
+
+    val notKenyan = false
+    println(notKenyan)
+}
+
+
